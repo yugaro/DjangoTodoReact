89b2fbdbb263af567c332872e06f4d9bddc48b92
yugaro <yugaro.yugaro@gmail.com>
2022-01-25 17:22:06 +0900

    Initial Commit

--- frontend/src/views/EditScreen.jsx
+++ frontend/src/views/EditScreen.jsx
@@ -14,30 +14,30 @@ export default function EditScreen() {
     completed: false,
   });
   const [todoList, setTodoList] = useState([
-    {
-      id: 1,
-      title: 'Go to Market',
-      description: 'Buy ingredients to prepare dinner',
-      completed: true,
-    },
-    {
-      id: 2,
-      title: 'Study',
-      description: 'Read Algebra and History textbook for the upcoming test',
-      completed: false,
-    },
-    {
-      id: 3,
-      title: "Sammy's books",
-      description: "Go to library to return Sammy's books",
-      completed: true,
-    },
-    {
-      id: 4,
-      title: 'Article',
-      description: 'Write article on how to use Django with React',
-      completed: false,
-    },
+    // {
+    //   id: 1,
+    //   title: 'Go to Market',
+    //   description: 'Buy ingredients to prepare dinner',
+    //   completed: true,
+    // },
+    // {
+    //   id: 2,
+    //   title: 'Study',
+    //   description: 'Read Algebra and History textbook for the upcoming test',
+    //   completed: false,
+    // },
+    // {
+    //   id: 3,
+    //   title: "Sammy's books",
+    //   description: "Go to library to return Sammy's books",
+    //   completed: true,
+    // },
+    // {
+    //   id: 4,
+    //   title: 'Article',
+    //   description: 'Write article on how to use Django with React',
+    //   completed: false,
+    // },
   ]);
 
   const createItem = () => {
